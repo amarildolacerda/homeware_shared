cdc2663e359ea5773d17529bb5a438950844bb34
Amarildo Lacerda <amarildo_51@msn.com>
2026-07-19 13:33:04 -0300

    Fix telnet crash: nao bloquear write quando buffer TCP cheio (evita WDT)

--- src/common_console.cpp
+++ src/common_console.cpp
@@ -34,13 +34,28 @@ int ConsoleOutput::telnet_read() {
 
 size_t ConsoleOutput::write(uint8_t c) {
     Serial.write(c);
-    if (m_client && m_client.connected()) m_client.write(c);
+    if (m_client && m_client.connected()) {
+        // Nao bloquear: se o buffer TCP do telnet estiver cheio (client nao
+        // consome), pular o byte em vez de travar o core (WDT reset).
+        if (m_client.availableForWrite() > 0)
+            m_client.write(c);
+        else
+            m_client.stop();
+    }
     return 1;
 }
 
 size_t ConsoleOutput::write(const uint8_t *buffer, size_t size) {
     Serial.write(buffer, size);
-    if (m_client && m_client.connected()) m_client.write((const char*)buffer, size);
+    if (m_client && m_client.connected()) {
+        int space = m_client.availableForWrite();
+        if (space > 0) {
+            if ((size_t)space > size) space = (int)size;
+            m_client.write((const char*)buffer, space);
+        } else {
+            m_client.stop();
+        }
+    }
     return size;
 }
 
